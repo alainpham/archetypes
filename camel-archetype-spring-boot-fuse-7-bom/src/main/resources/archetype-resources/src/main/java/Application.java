79a1296a1e53fd37ca4916bcd6a67b295d407b2d
alainpham <alainphm@gmail.com>
2018-06-05 18:24:21 +0200

    added app.java

---
+++ camel-archetype-spring-boot-fuse-7-bom/src/main/resources/archetype-resources/src/main/java/Application.java
@@ -0,0 +1,17 @@
+package ${package};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource({"classpath:spring/camel-context.xml"})
+public class Application {
+    // must have a main method spring-boot can run
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    
+    
+}

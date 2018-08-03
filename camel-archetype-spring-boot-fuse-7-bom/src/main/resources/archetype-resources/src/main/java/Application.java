183a14053f0e8347e408d0a375486036713196b2
alainpham <alainphm@gmail.com>
2018-08-03 12:02:48 +0200

    few fixes for git ignore

--- camel-archetype-spring-boot-fuse-7-bom/src/main/resources/archetype-resources/src/main/java/Application.java
+++ camel-archetype-spring-boot-fuse-7-bom/src/main/resources/archetype-resources/src/main/java/Application.java
@@ -1,17 +1,29 @@
 package ${package};
 
+import org.apache.cxf.transport.servlet.CXFServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
 @ImportResource({"classpath:spring/camel-context.xml"})
 public class Application {
+
+    @Value("${cxf.path}")
+    private String cxfPath;
+
     // must have a main method spring-boot can run
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    
-    
+    @Bean
+    public ServletRegistrationBean dispatcherServlet() {
+        return new ServletRegistrationBean(new CXFServlet(), cxfPath+"/*");
+    }
+
+
 }

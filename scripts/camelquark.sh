aa03b2e37c0ce7db1c0ceb5674eeba445a0bbefc
Alain Pham <alainphm@gmail.com>
2021-02-18 23:56:08 +0100

    fix some js pbs

--- scripts/camelquark.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-mvn archetype:generate -DarchetypeGroupId=io.github.alainpham -DarchetypeArtifactId=quarkus-camel -DarchetypeVersion=1.0.0
---
+++ scripts/copy-ui.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cp -R /home/workdrive/TAZONE/WORKSPACES/app-archetypes/js-front/static/* ../camel-archetype-spring-boot-2-fuse-7-bom/src/main/resources/archetype-resources/src/main/resources/static/
